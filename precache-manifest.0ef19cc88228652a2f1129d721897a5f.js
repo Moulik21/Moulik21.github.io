599a3694f07430ed152de2f18139a0e4944bd32c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-23 05:18:38 +0000

    Deploy Moulik21/moulik21.github.io to github.com/Moulik21/moulik21.github.io.git:gh-pages

---
+++ precache-manifest.0ef19cc88228652a2f1129d721897a5f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cedf8825aae24bd0ca0d4c8233c70b9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfdaece4e08e192c0a6e",
+    "url": "/static/css/2.59815e44.chunk.css"
+  },
+  {
+    "revision": "fdc06110a008df2f304a",
+    "url": "/static/css/main.30470fc7.chunk.css"
+  },
+  {
+    "revision": "cfdaece4e08e192c0a6e",
+    "url": "/static/js/2.4eb5143f.chunk.js"
+  },
+  {
+    "revision": "8236839320e1ee874fbacf7a3ecb44a0",
+    "url": "/static/js/2.4eb5143f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc06110a008df2f304a",
+    "url": "/static/js/main.717d73b5.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  }
+]);
